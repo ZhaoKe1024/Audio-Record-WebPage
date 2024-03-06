a94debad5439f6a72848b66e87d0bbb1f2abb6f5
ZhaoKe <57223477+ZhaoKe1024@users.noreply.github.com>
2024-03-06 12:42:01 +0800

    add sqlalchemy test

---
+++ databasekits/test_conn.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+
+def sqlalchemy_test():
+    ENGINE = create_engine("mysql+pymysql://root:zkGcx_0703@127.0.0.1:3306/cough_schema")
+    sql = """
+        select * from cough_main;
+        """
+    df = pd.read_sql(sql, ENGINE)
+    print(df)
+
+if __name__ == "__main__":
+    sqlalchemy_test()

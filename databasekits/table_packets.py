2e0da6f1affc96bb302581d4da09d81830245077
Zhaoke1024 <1355449245@qq.com>
2024-03-06 23:30:37 +0800

    update debug for backend

--- databasekits/table_packets.py
+++ databasekits/table_packets.py
@@ -4,7 +4,7 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy import Column
-from sqlalchemy import Integer, String, DateTime
+from sqlalchemy import Integer, SmallInteger, Boolean, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
@@ -15,7 +15,8 @@ pwd = "zkGcx_0703"
 
 class Item(Base):
     __tablename__ = 'cough_main'
-    id = Column(Integer, primary_key=True)  # primary key, id  # Even if this field is not used, it must be explicitly defined, otherwise it cannot be mapped to the data table.
+    id = Column(Integer,
+                primary_key=True)  # primary key, id  # Even if this field is not used, it must be explicitly defined, otherwise it cannot be mapped to the data table.
     filename = Column(String(32))  # # 文件名，自然数
     # health = Column(String(16))  # 是否健康，二值标记
     disease = Column(SmallInteger())  # 疾病类别，自然数标记
@@ -23,46 +24,45 @@ class Item(Base):
     age = Column(SmallInteger())  # 年龄  # 二位数
     issmoking = Column(Boolean())  # 是否抽烟？但是儿科医院肯定不抽  # 0-1 二值
     isfever = Column(Boolean())  # 是否抽烟？但是儿科医院肯定不抽  # 0-1 二值
+
     # from_device = Column(DateTime)  # 采集设备？这个其实是开集识别了  # 自然数
 
     def __repr__(self):
-        return "<Item(filename='%s', disease='%s'>" % \
-               self.filename, self.disease
+        return "<Item(filename='%s', disease='%s'>" % (self.filename, self.disease)
 
 
 # This function is called by the flask service
 def insert_use_dict(sess_dict):
-	ENGINE = create_engine(f"mysql+pymysql://root:{pwd}@127.0.0.1:3306/cough_schema")
-	Base.metadata.create_all(ENGINE)
-	Session = sessionmaker(bind=ENGINE)
-	session = Session()
-	try:
-		fname = "null" if "filename" not in sess_dict else sess_dict['filename']
-		disease_flag = -1 if "disease" not in sess_dict else sess_dict['fisease']
-		session.add_all([Item(filename=fname, disease=disease_flag, gender=sess_dict['gender'], 
-							age=sess_dict['age'], issmoking=sess_dict['issmoking'], isfever=sess_dict['isfever'])
-						])
-		session.commit()
-		print("Data insert into database cough_schema table cough_main successfully!")
-		sqlalchemy_test()
-		return True
-	except Exception as e:
-		print(e)
+    ENGINE = create_engine(f"mysql+pymysql://root:{pwd}@127.0.0.1:3306/cough_schema")
+    Base.metadata.create_all(ENGINE)
+    Session = sessionmaker(bind=ENGINE)
+    session = Session()
+    try:
+        fname = "null" if "filename" not in sess_dict else sess_dict['filename']
+        session.add_all([Item(filename=fname, disease=sess_dict['disease'], gender=sess_dict['gender'],
+                              age=sess_dict['age'], issmoking=sess_dict['issmoking'], isfever=sess_dict['isfever'])
+                         ])
+        session.commit()
+        print("Data insert into database cough_schema table cough_main successfully!")
+        sqlalchemy_test()
+        return True
+    except Exception as e:
+        print(e)
 
 
 def test_insert():
-	ENGINE = create_engine(f"mysql+pymysql://root:{pwd}@127.0.0.1:3306/cough_schema")
-	Base.metadata.create_all(ENGINE)
-	Session = sessionmaker(bind=ENGINE)
-	session = Session()
-	try:
-		session.add_all([Item(filename="test_no_pkey_0", disease=0, gender=0, age=23, issmoking=1, isfever=0)
-						])
-		session.commit()
-		print("Data insert into database cough_schema table cough_main successfully!")
-		sqlalchemy_test()
-	except Exception as e:
-		print(e)
+    ENGINE = create_engine(f"mysql+pymysql://root:{pwd}@127.0.0.1:3306/cough_schema")
+    Base.metadata.create_all(ENGINE)
+    Session = sessionmaker(bind=ENGINE)
+    session = Session()
+    try:
+        session.add_all([Item(filename="test_no_pkey_0", disease=0, gender=0, age=23, issmoking=1, isfever=0)
+                         ])
+        session.commit()
+        print("Data insert into database cough_schema table cough_main successfully!")
+        sqlalchemy_test()
+    except Exception as e:
+        print(e)
 
 
 def sqlalchemy_test():
@@ -77,4 +77,4 @@ def sqlalchemy_test():
 
 if __name__ == '__main__':
     test_insert()
-	# sqlalchemy_test()
+# sqlalchemy_test()
--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 # @Software: PyCharm
 from flask import Flask, request, jsonify, render_template
 
+from databasekits.table_packets import insert_use_dict
+
 app = Flask(__name__)
 
 
@@ -18,16 +20,28 @@ def index():
 # 定义上传音频文件的路由
 @app.route('/getdata', methods=['POST'])
 def upload():
-	info_table = request.get_json()
-	print(info_table)
-	# audio_file = request.files['audio']
-	# # 将音频文件保存到服务器上的指定路径
-	# audio_file.save('./audio/test_audio_000.wav')
-	resp_message = "Data received successfully!\n"
-	insert_use_dict(info_table)
-	resp_message += "Data insert into database successfully!"
-	response = {'message': resp_message}
-	return jsonify(response)
+    info_table = request.get_json()
+    print(info_table)
+    # audio_file = request.files['audio']
+    # # 将音频文件保存到服务器上的指定路径
+    # audio_file.save('./audio/test_audio_000.wav')
+    resp_message = "Data received successfully!\n"
+    insert_use_dict(info_table)
+    resp_message += "Data insert into database successfully!"
+    response = {'message': resp_message}
+    return jsonify(response)
+
+
+@app.route('/test', methods=['POST'])
+def print_dcit():
+    info_table = request.get_json()
+    print(info_table)
+    # audio_file = request.files['audio']
+    # # 将音频文件保存到服务器上的指定路径
+    # audio_file.save('./audio/test_audio_000.wav')
+    resp_message = "Data received successfully!\n"
+    response = {'message': resp_message}
+    return jsonify(response)
 
 
 if __name__ == '__main__':

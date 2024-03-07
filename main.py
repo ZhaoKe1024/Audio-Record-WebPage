85ac4333425ce4a0542030ea77e370eb60e21aaf
Zhaoke1024 <1355449245@qq.com>
2024-03-07 16:23:51 +0800

    update study

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # @Author: ZhaoKe
 # @File : main.py
 # @Software: PyCharm
+import base64
+
 import numpy as np
 import soundfile
 from flask import Flask, request, jsonify, render_template
@@ -22,8 +24,8 @@ def index():
 # 定义上传音频文件的路由
 @app.route('/getdata', methods=['POST'])
 def upload():
-    info_table = request.get_json()
-    print(info_table)
+    info_table = request.form
+    print(info_table['jsondata'])
     samples = np.frombuffer(info_table['audio'], dtype=np.int16)
     fname = "test_audio_000.wav"
     soundfile.write(
@@ -45,12 +47,39 @@ def upload():
 
 @app.route('/test', methods=['POST'])
 def print_dcit():
-    info_table = request.get_json()
-    print(info_table)
+    print("收到信息！")
+    info_table = None
+    try:
+        info_table = request.form
+        print(info_table)
+    except Exception as e:
+        print(e)
+        print("Error at request.form")
+    print("采样率：", info_table['sr'])
+    sr = info_table['sr']
+    print("信息：", info_table["jsondata"])
+    samples = None
+    try:
+        audio_data = info_table.get('audio')
+        binary_audio = base64.b64decode(audio_data)
+        samples = np.frombuffer(binary_audio, dtype=np.int32)
+        print("duration：", len(samples))
+    except Exception as e:
+        print(e)
+        print("Error at samples = np.frombuffer(info_table['audio'], dtype=np.int16)")
+    fname = "test_audio_000.wav"
+    soundfile.write(
+        f"./audio/{fname}",
+        samples,
+        16000,
+        format='WAV',
+        subtype="FLOAT")
     # audio_file = request.files['audio']
     # # 将音频文件保存到服务器上的指定路径
     # audio_file.save('./audio/test_audio_000.wav')
     resp_message = "Data received successfully!\n"
+    info_table['jsondata']['filename'] = fname
+    print("filename", fname)
     response = {'message': resp_message}
     return jsonify(response)
 

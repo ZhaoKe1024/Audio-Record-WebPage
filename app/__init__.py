326b624432a7b2d7290539a006ffd95b62488e82
Zhaoke1024 <1355449245@qq.com>
2024-03-08 19:47:02 +0800

    update opload method by chunk

--- app/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author : ZhaoKe
-# @Time : 2024-03-07 18:20
--- main.py
+++ main.py
@@ -4,34 +4,69 @@
 # @Author: ZhaoKe
 # @File : main.py
 # @Software: PyCharm
-import base64
-
+import os
 import numpy as np
 import soundfile
 from flask import Flask, request, jsonify, render_template
-from flask_uploads import UploadSet, configure_uploads, patch_request_class
 from databasekits.table_packets import insert_use_dict
 
 app = Flask(__name__)
-from app import routes
 
-# # 配置文件上传
-# app.config['UPLOADS_DEFAULT_DEST'] = 'static/uploads'
-# app.config['UPLOADS_DEFAULT_URL'] = '/static/uploads'
-# app.config['UPLOADSet'] = ('my_uploads', 'uploads', 'all')
-
-# 初始化上传套件
-# my_uploads = UploadSet(app)
-# configure_uploads(app, my_uploads)
-# patch_request_class(app)  # 据说默认16MB
+CHUNK_SIZE = 1024 * 1024
 MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # 20M
-patch_request_class(app, MAX_CONTENT_LENGTH)
 
 
 @app.route('/')
 def index():
     return render_template("./index.html")
     # return "<p>hello world</p><br><p>ok!</p>"
+
+
+total_chunks = 1024
+@app.route('/merge', methods=['POST'])
+def merge_chunks():
+    filename = request.form.get('filename')
+    chunk_dir = './recorded_audio'  # 存放分块文件的目录
+    chunk_paths = [os.path.join(chunk_dir, filename + '_' + str(i)) for i in range(total_chunks)]
+    try:
+        with open(filename, 'wb') as f:
+            for chunk_path in chunk_paths:
+                with open(chunk_path, 'rb') as chunk_file:
+                    f.write(chunk_file.read())
+                os.remove(chunk_path)  # 删除已经合并的分块文件
+        return jsonify({'code': 0, 'message': '上传成功'})
+    except Exception as e:
+        print(e)
+        print("分块合并失败！")
+        return jsonify({'code': -1, 'message': "失败："+str(e)})
+
+
+@app.route('/postchunk', methods=['POST'])
+def get_chunk():
+    try:
+        chunk = request.form.get('chunkIndex')  # 获取当前上传的块数
+        filename = request.form.get('filename')  # 获取文件名
+        print(f"chunk filename: {chunk}, {filename}")
+        file = request.form.get('file')  # 获取上传的文件  # 这里是args不是files
+
+        if not file or not chunk or not filename:
+            return jsonify({'code': -1, 'message': '缺少参数'})
+
+        # 创建文件夹用来存储分块
+        upload_dir = './temp_files'
+        if not os.path.exists(upload_dir):
+            os.makedirs(upload_dir)
+
+        # 将分块保存到指定的位置
+        chunk_file = os.path.join(upload_dir, filename + '.' + str(chunk))
+        with open(chunk_file, 'wb') as f:
+            f.write(file.read())
+        print(f"保存分块:{filename}.{chunk}")
+        return jsonify({'code': 0, 'message': '上传成功'})
+    except Exception as e:
+        print(e)
+        print(f"分块上传失败！")
+        return jsonify({'code': -1, 'message': str(e)})
 
 
 # 定义上传音频文件的路由
@@ -61,40 +96,14 @@ def upload():
 @app.route('/test', methods=['POST'])
 def print_dcit():
     print("收到信息！")
-    info_table = None
     try:
         info_table = request.form
         print(info_table)
     except Exception as e:
         print(e)
         print("Error at request.form")
-    print("采样率：", info_table['sr'])
-    sr = info_table['sr']
-    print("信息：", info_table["jsondata"])
-    samples = None
-    try:
-        audio_data = info_table.get('audio')
-        binary_audio = base64.b64decode(audio_data)
-        samples = np.frombuffer(binary_audio, dtype=np.int32)
-        print("duration：", len(samples))
-    except Exception as e:
-        print(e)
-        print("Error at samples = np.frombuffer(info_table['audio'], dtype=np.int16)")
-    fname = "test_audio_000.wav"
-    soundfile.write(
-        f"./audio/{fname}",
-        samples,
-        16000,
-        format='WAV',
-        subtype="FLOAT")
-    # audio_file = request.files['audio']
-    # # 将音频文件保存到服务器上的指定路径
-    # audio_file.save('./audio/test_audio_000.wav')
-    resp_message = "Data received successfully!\n"
-    info_table['jsondata']['filename'] = fname
-    print("filename", fname)
-    response = {'message': resp_message}
-    return jsonify(response)
+    response = {'code': 0, 'message': "table form received successfully!"}
+    return jsonify(response=response)
 
 
 if __name__ == '__main__':
